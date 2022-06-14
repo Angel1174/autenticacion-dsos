456608ecf41f5961858f831254b06192a7943471
Angel1174 <angel.ricardo117@gmail.com>
2022-06-14 14:09:33 -0500

    cam

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import Recuperar from './views/Recuperar';
 import { EnviarEmail } from './views/EnviarEmail';
 import {
   //HashRouter as Router,
-  BrowserRouter as Router,
+  //BrowserRouter as Router,
   HashRouter,
   Routes,
   Route,
+  //BrowserRouter,
 } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HashRouter basename={''}>
-
+      <HashRouter basename={``}>
         <Routes>
-          <Route exact path="/autenticacion-dsos/usuarios" element={<Principal />}>
+          <Route path="/autenticacion-dsos/usuarios" element={<Principal />}>
           </Route>
-          <Route exact path="/autenticacion-dsos/recuperar" element={<Recuperar />}>
+          <Route path="/autenticacion-dsos/recuperar" element={<Recuperar />}>
           </Route>
-          <Route exact path="/autenticacion-dsos/registrar" element={<EnviarEmail />}>
+          <Route  path="/autenticacion-dsos/registrar" element={<EnviarEmail />}>
           </Route>
           <Route exact path="/autenticacion-dsos" element={<Login />}>
           </Route>
