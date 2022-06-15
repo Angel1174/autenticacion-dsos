e19dabff211ada9b3fee9ee478e1727827efefd4
Angel1174 <angel.ricardo117@gmail.com>
2022-06-15 09:59:45 -0500

    cambio en login

--- src/views/Login.js
+++ src/views/Login.js
@@ -72,7 +72,7 @@ class Login extends React.Component {
                             <input type="password" className="fadeIn third" name="password" placeholder="Contraseña" onChange={this.handleChange} />
                             <br></br>
                             <br></br>
-                            <input type="submit" value="Actualizar datos" onClick={() => this.iniciarSesion()} />
+                            <input type="submit" value="Iniciar sesión" onClick={() => this.iniciarSesion()} />
                         </form>
                         <Link className="nav-link" to={"/recuperar"}><span className="material-icons">
                             ¿Necesitas actualizar datos?, click aquí
